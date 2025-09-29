a434265ccaf6eb7d873147b12e25444804c8f399
Kristian Gøystdal <krisgoy100@gmail.com>
2025-09-29 10:06:02 +0200

    Update user_config.h to set initial RTC time and enable watermark values

--- STM32/Core/Inc/user_config.h
+++ STM32/Core/Inc/user_config.h
@@ -6,19 +6,18 @@
 //-------------------------------------------------------//
 
 // Configuration for first time booting up the RTC
-#define SET_INITIAL_TIME false // Set to true to set the initial time
-#define INITIAL_HOUR 12
-#define INITIAL_MIN 0
+#define SET_INITIAL_TIME true // Set to true to set the initial time
+#define INITIAL_HOUR 9
+#define INITIAL_MIN 55
 #define INITIAL_SEC 0
-#define INITIAL_DOW 4    // Day of week (1=Mon ... 7=Sun)
-#define INITIAL_DOM 1    // Day of month (1–31)
-#define INITIAL_MONTH 1  // Month (1–12)
-#define INITIAL_YEAR 2024 // Year (e.g. 2024)
+#define INITIAL_DOW 1   // Day of week (1=Mon ... 7=Sun)
+#define INITIAL_DOM 29    // Day of month (1–31)
+#define INITIAL_MONTH 9  // Month (1–12)
+#define INITIAL_YEAR 2025 // Year (e.g. 2024)
 
 // Configuration for the interval between repeats
 #define USE_DEFAULT_INTERVAL_BETWEEN_REPEATS true // Default is 60 seconds
 #define USE_MINUTES_INSTEAD_OF_SECONDS true
-#define INTERVAL_BETWEEN_REPEATS_SECONDS 5
 #define INTERVAL_BETWEEN_REPEATS_MINUTES 10
 
 // Configuration for values to include in the watermark
@@ -28,9 +27,9 @@
 #define TEMPERATURE 20
 
 // Configuration for toggles of values to include in the watermark
-#define INCLUDE_USER_STRING false
-#define INCLUDE_DEVICE_ID false
-#define INCLUDE_LOCATION false
+#define INCLUDE_USER_STRING true
+#define INCLUDE_DEVICE_ID true
+#define INCLUDE_LOCATION true
 #define INCLUDE_TEMPERATURE true
 #define INCLUDE_TIME true // Current time from RTC
 
