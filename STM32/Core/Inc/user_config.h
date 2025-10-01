f6fc1a26ca0c7bd176d2c287c41637313a90fda0
Kristian Gøystdal <42619784+kristiangoystdal@users.noreply.github.com>
2025-10-01 15:24:36 +0200

    27 delay message (#31)
    * Remove DAC initialization code and force TIM8 to overflow immediately
    
    * Add timestamp information for audio segments in main execution
    
    * Add configuration for the initial minute to start transmissions
    
    * Refactor TIM8 callback to handle initial minute configuration and DAC operations

--- STM32/Core/Inc/user_config.h
+++ STM32/Core/Inc/user_config.h
@@ -15,6 +15,9 @@
 #define INITIAL_MONTH 9  // Month (1–12)
 #define INITIAL_YEAR 2025 // Year (e.g. 2024)
 
+// Configuration for which minute in the hour to start transmissions (0-59)
+#define INITIAL_MINUTE 0 
+
 // Configuration for the interval between repeats
 #define USE_DEFAULT_INTERVAL_BETWEEN_REPEATS true // Default is 60 seconds
 #define USE_MINUTES_INSTEAD_OF_SECONDS true
