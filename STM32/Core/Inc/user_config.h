267e7fb9bffd9c26bf56e6bb345fedf41155d454
Kristian Gøystdal <42619784+kristiangoystdal@users.noreply.github.com>
2025-10-01 15:49:20 +0200

    Refactor (#32)
    * refactor user_config.h for improved formatting and readability
    
    * refactor main.c for improved readability and consistency

--- STM32/Core/Inc/user_config.h
+++ STM32/Core/Inc/user_config.h
@@ -10,9 +10,9 @@
 #define INITIAL_HOUR 15
 #define INITIAL_MIN 0
 #define INITIAL_SEC 0
-#define INITIAL_DOW 2  // Day of week (1=Mon ... 7=Sun)
+#define INITIAL_DOW 2     // Day of week (1=Mon ... 7=Sun)
 #define INITIAL_DOM 30    // Day of month (1–31)
-#define INITIAL_MONTH 9  // Month (1–12)
+#define INITIAL_MONTH 9   // Month (1–12)
 #define INITIAL_YEAR 2025 // Year (e.g. 2024)
 
 // Configuration for which minute in the hour to start transmissions (0-59)
@@ -20,7 +20,6 @@
 
 // Configuration for the interval between repeats
 #define USE_DEFAULT_INTERVAL_BETWEEN_REPEATS true // Default is 60 seconds
-#define USE_MINUTES_INSTEAD_OF_SECONDS true
 #define INTERVAL_BETWEEN_REPEATS_MINUTES 10
 
 // Configuration for values to include in the watermark
