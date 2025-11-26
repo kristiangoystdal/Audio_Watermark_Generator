b255c162e7bf72779a7b94475a1c40dce016a5a3
Kristian Gøystdal <krisgoy100@gmail.com>
2025-11-26 14:20:29 +0100

    Update FSK frequency pair to default and ensure transmission methods are enabled

--- STM32/Core/Inc/user_config.h
+++ STM32/Core/Inc/user_config.h
@@ -42,6 +42,10 @@
 // 2 = 12.5kHz and 11.1kHz
 // 3 = 8.3kHz and 6.9kHz
 // 4 = 2.8kHz and 1.4kHz
-#define FSK_FREQUENCY_PAIR 4
+#define FSK_FREQUENCY_PAIR 1
+
+// Configuration for transmission method
+#define USE_CABLE_DRIVEN_TRANSMISSION true
+#define USE_SPEAKER_DRIVEN_TRANSMISSION true
 
 #endif // USER_CONFIG_H
