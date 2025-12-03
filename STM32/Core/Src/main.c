f58128703c5b851026db218a1fd11f646b60d63e
Kristian Gøystdal <krisgoy100@gmail.com>
2025-12-03 10:46:55 +0100

    Fix GPIO pin usage for cable transmission in PWM callback

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -485,9 +485,9 @@ void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
     }
     if (USE_CABLE_TRANSMISSION) {
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
       DWT_Delay_ms(100);
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
     }
 
     tx_active = false;
