59bf8a942dcabf030e46fb9a87b6cbda745c7345
Kristian Gøystdal <42619784+kristiangoystdal@users.noreply.github.com>
2026-01-20 15:38:24 +0100

    84 better sleep mode (#93)
    * Implement alarm functionality and GPIO interrupt for DS3231 RTC
    
    * Add .gitignore file to exclude build artifacts and temporary files
    
    * Init files for a new build
    
    * Update project configuration for improved GPIO and timer settings
    
    * Enhance sleep mode functionality and timer integration
    
    - Removed unused includes and adjusted configuration for better performance.
    - Added TIM6 initialization and interrupt handling for precise timing control.
    - Implemented EXTI interrupt for GPIO pin to wake from sleep mode.
    - Updated GPIO initialization to support new features and low power modes.
    
    * Add user configuration header for RTC and watermark settings
    
    * Implement bitstream generation and input string update functions
    
    * Add DAC and DMA configuration to project settings for enhanced audio processing
    
    * Add FSK frequency definitions for lower and higher frequency settings
    
    * Enable DAC module and add DMA configuration for DAC1, including interrupt handlers
    
    * Add DAC and DMA initialization, sine wave generation, and bitstream handling functions
    
    * 88 better selection of frequency pairs (#91)
    
    * Refactor FSK frequency configuration by removing frequency pair options and defining specific lower and higher frequencies
    
    * Refactor FSK processing by introducing dynamic sample and period calculations for lower and higher frequencies
    
    * Add CMake install script for STM32CubeMX configuration
    
    * Add DC mid-level value calculation and update FSK structure
    
    * Add RTC wake flag and comment out alarm configuration
    
    * Refactor GPIO pin configuration, enable COM features, and enhance main loop for better sleep mode handling
    
    * Refactor DMA configuration to use word alignment and update data types for improved performance
    
    * Update FSK frequency values for improved transmission quality
    
    * Refactor interrupt handler function headers for consistency and clarity
    
    * Refactor transmission flow and improve DAC handling for better sleep mode performance
    
    * Add DS3231 RTC driver with power control, time setting, and alarm functions
    
    * Enable I2C module and add MSP initialization and de-initialization functions for I2C2
    
    * Add I2C2 configuration and update project settings for enhanced functionality
    
    * Add I2C2 initialization and DS3231 integration for improved timekeeping functionality

---
+++ STM32_New/Core/Inc/user_config.h
@@ -0,0 +1,56 @@
+#include <stdbool.h>
+
+#ifndef USER_CONFIG_H
+#define USER_CONFIG_H
+
+//------------------ USER CONFIGURATION ------------------//
+// Modify these values as needed
+//-------------------------------------------------------//
+
+// Configuration for first time booting up the RTC
+#define SET_INITIAL_TIME false
+#define INITIAL_HOUR 12
+#define INITIAL_MIN 54
+#define INITIAL_SEC 12
+#define INITIAL_DOW 3
+#define INITIAL_DOM 8
+#define INITIAL_MONTH 10
+#define INITIAL_YEAR 2025
+
+// Configuration for which minute in the hour to start transmissions (0-59)
+#define ENABLE_DELAYED_START false
+#define STARTING_MINUTE 0
+
+// Configuration for the interval between repeats
+#define USE_DEFAULT_INTERVAL_BETWEEN_REPEATS true // Default is 60 seconds
+#define INTERVAL_BETWEEN_REPEATS_MINUTES 10
+
+// Configuration for values to include in the watermark
+#define USER_STRING "Hello World"
+#define DEVICE_ID 42
+#define LOCATION "63.4190,10.4015"
+#define TEMPERATURE 20
+
+// Configuration for toggles of values to include in the watermark
+#define INCLUDE_USER_STRING true
+#define INCLUDE_DEVICE_ID true
+#define INCLUDE_LOCATION true
+#define INCLUDE_TEMPERATURE true
+#define INCLUDE_TIME true
+
+// Configuration for transmission method
+#define USE_CABLE_TRANSMISSION true
+#define USE_SPEAKER_TRANSMISSION true
+
+// Configuration for FSK frequency pair
+// Options:
+// 1 = 20.8kHz and 22.2kHz (Default)
+// 2 = 12.5kHz and 11.1kHz
+// 3 = 8.3kHz and 6.9kHz
+// 4 = 2.8kHz and 1.4kHz
+#define FSK_FREQUENCY_PAIR 1
+
+#define FSK_LOWER_FREQUENCY 20884
+#define FSK_HIGHER_FREQUENCY 22222
+
+#endif // USER_CONFIG_H
