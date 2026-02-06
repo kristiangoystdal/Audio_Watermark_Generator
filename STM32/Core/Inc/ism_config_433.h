8e4e014a4e513e8688cd388c83ce2e76580f1962
Kristian Gøystdal <krisgoy100@gmail.com>
2026-02-06 11:25:20 +0100

    Update CC1101 RX and TX frequency settings in ism_config_433.h

--- STM32/Core/Inc/ism_config_433.h
+++ STM32/Core/Inc/ism_config_433.h
@@ -7,9 +7,9 @@ static const ism_reg_t cc1101_cfg_rx[] = {
     {0x04, 0x8C}, // Set sync word (SYNC1)
     {0x05, 0x36}, // Set sync word (SYNC0)
     {0x12, 0x03}, // MDMCFG2: 30/32 sync bits detected
-    {0x0D, 0x10}, // FREQ2
-    {0x0E, 0x93}, // FREQ1
-    {0x0F, 0x89}, // FREQ
+    {0x0D, 0x13}, // FREQ2
+    {0x0E, 0x3E}, // FREQ1
+    {0x0F, 0x98}, // FREQ
     {0x02, 0x06}, // IOCFG0: GDO0 asserts on sync, deassert end/FIFO overflow
     {0x08, 0x01}, // Set device address to 0x42
     {0x03, 0x37}, // Enable address check
@@ -19,11 +19,9 @@ static const ism_reg_t cc1101_cfg_tx[] = {
     {0x02, 0x06}, // IOCFG0: GDO0 asserts on sync, deassert end/FIFO overflow
     {0x04, 0x04}, // Set sync word (SYNC1)
     {0x05, 0x05}, // Set sync word (SYNC0)
-    {0x12, 0x03}, // MDMCFG2: 30/32 sync bits detected
     {0x0D, 0x10}, // FREQ2
-    {0x0E, 0x93}, // FREQ1
-    {0x0F, 0x89}, // FREQ
-    {0x08, 0x01}, // Enable variable length mode
+    {0x0E, 0xAA}, // FREQ1
+    {0x0F, 0x6E}, // FREQ
 };
 
 static const ism_reg_t cc1101_tx[] = {
