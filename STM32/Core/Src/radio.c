dce68345b134383df83fe62aa93531702b51fb2a
Kristian Gøystdal <krisgoy100@gmail.com>
2026-03-18 16:00:45 +0100

    Fix TX FIFO write error check to match expected status code

--- STM32/Core/Src/radio.c
+++ STM32/Core/Src/radio.c
@@ -137,7 +137,7 @@ void transmit_bytes(void) {
 
   // Burst write into TX FIFO
   CC1101_WriteBurstReg(0x3F, pkt, sizeof(pkt), &status);
-  if (status != 0) {
+  if (status != 0x0F) {
     LOGF("Error writing to TX FIFO, status: 0x%02X\r\n", status);
     // Error_Handler_Code(STATUS_CODE_TRANSMISSION_ERROR);
     return;
