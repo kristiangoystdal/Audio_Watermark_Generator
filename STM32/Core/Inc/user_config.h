1224429e0737d826f6a317c8d46cf235567b8be4
Kristian Gøystdal <krisgoy100@gmail.com>
2026-03-18 16:03:22 +0100

    Refactor user configuration for base station timing and transmission intervals

--- STM32/Core/Inc/user_config.h
+++ STM32/Core/Inc/user_config.h
@@ -17,14 +17,6 @@
 #define INITIAL_MONTH 10
 #define INITIAL_YEAR 2025
 
-// Configuration for which minute in the hour to start transmissions (0-59)
-#define ENABLE_DELAYED_START false
-#define STARTING_MINUTE 0
-
-// Configuration for the interval between repeats
-#define USE_DEFAULT_INTERVAL_BETWEEN_REPEATS true // Default is 60 seconds
-#define INTERVAL_BETWEEN_REPEATS_MINUTES 10
-
 // Configuration for values to include in the watermark
 #define USER_STRING "Hello World"
 #define DEVICE_ID 42
@@ -45,15 +37,32 @@
 // Configuration for FSK frequencies (in Hz) - these will be adjusted by the
 // algorithm if they don't meet the sample count requirements, but should be set
 // close to the desired frequencies to minimize adjustments
-#define FSK_LOWER_FREQUENCY 21333
-#define FSK_HIGHER_FREQUENCY 21818
+#define FSK_LOWER_FREQUENCY 18461
+#define FSK_HIGHER_FREQUENCY 19200
 
 // Configuration for attenuation of the signal in percent (0-100), where 100 is
 // no attenuation and 0 is maximum attenuation (silence)
 #define SIGNAL_ATTENUATION 100
 
-// Configuration for whether to use Reed-Solomon error correction coding, which adds redundancy to the transmitted data to allow for error detection and correction at the receiver. This can improve reliability in noisy environments but reduces the effective data rate due to the added redundancy.
+// Configuration for whether to use Reed-Solomon error correction coding, which
+// adds redundancy to the transmitted data to allow for error detection and
+// correction at the receiver. This can improve reliability in noisy
+// environments but reduces the effective data rate due to the added redundancy.
 #define USE_REED_SOLOMON_ERROR_CORRECTION true
 #define RS_ERROR_CORRECTION_SYMBOLS 20
+
+// Configuration for whether to start in RX mode (listening for incoming
+// signals) or TX mode (transmitting signals)
+#define RX_MODE false
+
+// Configuration for base station transmission intervals and timing
+// If delayed start is enabled, the device will wait until the specified
+// starting time is reached
+#define ENABLE_DELAYED_START false
+#define STARTING_MINUTE 0
+
+// Configuration for the interval between repeats
+#define USE_DEFAULT_INTERVAL_BETWEEN_REPEATS true // Default is 60 seconds
+#define INTERVAL_BETWEEN_REPEATS_MINUTES 10
 
 #endif // USER_CONFIG_H
