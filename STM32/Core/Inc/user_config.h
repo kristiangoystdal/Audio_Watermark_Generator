b1f5ba959d9c1d78cb4f00a9336ccce523a1a50f
Kristian Gøystdal <krisgoy100@gmail.com>
2026-03-19 11:00:14 +0100

    Enable RX mode for base station configuration

--- STM32/Core/Inc/user_config.h
+++ STM32/Core/Inc/user_config.h
@@ -53,7 +53,7 @@
 
 // Configuration for whether to start in RX mode (listening for incoming
 // signals) or TX mode (transmitting signals)
-#define RX_MODE false
+#define RX_MODE true
 
 // Configuration for base station transmission intervals and timing
 // If delayed start is enabled, the device will wait until the specified
