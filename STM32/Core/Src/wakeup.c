67f4641cafb73e0a0026c2da88f380583be544ca
Kristian Gøystdal <krisgoy100@gmail.com>
2026-04-30 10:10:10 +0200

    Refactor logging in wakeup timer functions for clarity and consistency

--- STM32/Core/Src/wakeup.c
+++ STM32/Core/Src/wakeup.c
@@ -25,25 +25,24 @@ void WakeUp_SetCurrentMinute(uint8_t minute) { current_minute = minute; }
 void WakeUp_CalculateNextValidMinute(void) {
   if (next_valid_minute == UNINITIALIZED) {
     if (ENABLE_DELAYED_START && current_minute != STARTING_MINUTE) {
-      LOGF("Setting next valid minute to starting minute %02d\r\n",
-           STARTING_MINUTE);
+      // LOGF("Setting next valid minute to starting minute %02d\r\n",
+      //      STARTING_MINUTE);
       next_valid_minute = STARTING_MINUTE;
       return;
 
     } else {
 
-      LOGF("Calculating next valid minute based on current minute %02d and "
-           "configured interval of %u minutes\r\n",
-           current_minute, (unsigned int)INTERVAL_BETWEEN_REPEATS_MINUTES);
-      next_valid_minute =
-          (current_minute + INTERVAL_BETWEEN_REPEATS_MINUTES) % 60;
+      LOGF("No valid minute configured, setting next valid minute to current "
+           "minute "
+           "+ 1 (wraps around at 60)\r\n");
+      next_valid_minute = (current_minute + 1) % 60;
 
       return;
     }
   } else {
-    LOGF("Calculating next valid minute based on last valid minute %02d and "
-         "configured interval of %u minutes\r\n",
-         next_valid_minute, (unsigned int)INTERVAL_BETWEEN_REPEATS_MINUTES);
+    // LOGF("Calculating next valid minute based on last valid minute %02d and "
+    //      "configured interval of %u minutes\r\n",
+    //      next_valid_minute, (unsigned int)INTERVAL_BETWEEN_REPEATS_MINUTES);
     next_valid_minute =
         (next_valid_minute + INTERVAL_BETWEEN_REPEATS_MINUTES) % 60;
   }
@@ -51,26 +50,26 @@ void WakeUp_CalculateNextValidMinute(void) {
 
 bool WakeUp_CheckCurrentMinute(rtc_time_t now) {
   WakeUp_SetCurrentMinute(now.minutes);
-  LOGF("Current minute: %02d\r\n", current_minute);
+  // LOGF("Current minute: %02d\r\n", current_minute);
 
   bool is_valid_minute = (current_minute == next_valid_minute);
 
   if (is_valid_minute) {
-    LOGF("Valid minute hit!\r\n");
+    // LOGF("Valid minute hit!\r\n");
     if (INTERVAL_BETWEEN_REPEATS_MINUTES > 5) {
       active_until_minute = (current_minute + 4) % 60;
     }
     WakeUp_CalculateNextValidMinute();
-    LOGF("Next valid minute after this window: %02d\r\n", next_valid_minute);
+    // LOGF("Next valid minute after this window: %02d\r\n", next_valid_minute);
   }
 
   if (active_until_minute != UNINITIALIZED) {
     if (current_minute > active_until_minute) {
-      LOGF("Active window ended.\r\n");
+      // LOGF("Active window ended.\r\n");
       active_until_minute = UNINITIALIZED;
       return false;
     }
-    LOGF("Currently in active window.\r\n");
+    // LOGF("Currently in active window.\r\n");
     return true;
   }
 
