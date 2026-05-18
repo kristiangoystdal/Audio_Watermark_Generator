704d4847256c3a23b7fde3143ecc60b30580009b
Kristian Gøystdal <krisgoy100@gmail.com>
2026-05-18 13:33:32 +0200

    Refactor relay and speaker configurations for consistency and update toggle delay

--- STM32/Core/Inc/relay.h
+++ STM32/Core/Inc/relay.h
@@ -9,6 +9,11 @@ extern "C" {
 #include <stdint.h>
 #include <stm32g4xx_hal.h>
 
+#define RELAY_GPIO_Port GPIOA
+#define RELAY_PIN_ON GPIO_PIN_6
+#define RELAY_PIN_OFF GPIO_PIN_5
+#define RELAY_TOGGLE_DELAY_MS 250
+
 void Relay_SetMixingMode(void);
 void Relay_SetBypassMode(void);
 
--- STM32/Core/Src/relay.c
+++ STM32/Core/Src/relay.c
@@ -3,11 +3,6 @@
 
 #include <stm32g4xx_hal.h>
 
-#define RELAY_GPIO_Port GPIOA
-#define RELAY_PIN_ON GPIO_PIN_6
-#define RELAY_PIN_OFF GPIO_PIN_5
-#define RELAY_TOGGLE_DELAY_MS 140
-
 void Relay_ResetBoth(void) {
   HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_PIN_ON, GPIO_PIN_RESET);
   HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_PIN_OFF, GPIO_PIN_RESET);
@@ -26,6 +21,6 @@ void Relay_SetBypassMode(void) {
   Relay_ResetBoth();
 
   HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_PIN_OFF, GPIO_PIN_SET);
-  HAL_Delay(RELAY_TOGGLE_DELAY_MS);
+  HAL_Delay(10);
   HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_PIN_OFF, GPIO_PIN_RESET);
 }
